0cd2ce65e32c5259cedd650bdbb250bfe9bee7fd
Lucas Lopes <lp.skt25@gmail.com>
2025-11-29 00:43:23 -0300

    chore: Implementantado estrutura inicial da API

---
+++ Backend/FinanceManager-API/src/main/java/com/lpdev/financemanagerapi/FinanceManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lpdev.financemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagerApiApplication.class, args);
+    }
+
+}
---
+++ Backend/FinanceManager-API/src/test/java/com/lpdev/financemanagerapi/FinanceManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpdev.financemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
